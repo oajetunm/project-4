7862fe8f6ff428d777651086e786fa3ea8d6a1c8
Olubisi <oajetunm@umich.edu>
2017-04-15 08:06:13 -0400

    commit message

--- model.py
+++ model.py
@@ -1,23 +1,72 @@
 
 import csv
+import operator
 
 data_is_loaded = False
 
 def load_data():
-
-	with open('US_County_Level_Presidential_Results_12-16.csv', 'r') as csvfile:
-	    reader = csv.reader(csvfile, delimiter=',')
-	    for row in reader:
-	    	pass # process the data
-	    data_is_loaded = True
+    dg ={}
+    gd = {}
+	# with open('US_County_Level_Presidential_Results_12-16.csv', 'r') as csvfile:
+	# 	reader = csv.DictReader(csvfile, delimiter=',')
+	# 	for row in reader:
+	# 		pass
+        #     if row['state_abbr'] in d:
+        #         d[row['state_abbr']].append(float(row['votes_dem_2016']))
+        #     else:
+        #         d[row['state_abbr']] = [float(row['votes_dem_2016'])]
+        #     d1 = dict((key, sum(vals)) for key, vals in d.items())
+        #     if row['state_abbr'] in g:
+        #         g[row['state_abbr']].append(float(row['votes_gop_2016']))
+        #     else:
+        #         g[row['state_abbr']] = [float(row['votes_gop_2016'])]
+        #     g1 = dict((key, sum(vals)) for key, vals in g.items())
+		#data_is_loaded = True
 
 def get_data(party='dem', raw=True, sort_ascending=True, year=2016):
-	if not data_is_loaded:
-		load_data()
+	d ={}
+	g = {}
+	with open('US_County_Level_Presidential_Results_12-16.csv', 'r') as csvfile:
+		reader = csv.DictReader(csvfile, delimiter=',')
+		for row in reader:
+			if row['state_abbr'] in d:
+				d[row['state_abbr']].append(float(row['votes_dem_2016']))
+			else:
+				d[row['state_abbr']] = [float(row['votes_dem_2016'])]
+			d1 = dict((key, sum(vals)) for key, vals in d.items())
+			#sorted_d1 = sorted(d1.items(), key=operator.itemgetter(1))
+			if row['state_abbr'] in g:
+				g[row['state_abbr']].append(float(row['votes_gop_2016']))
+			else:
+				g[row['state_abbr']] = [float(row['votes_gop_2016'])]
+			g1 = dict((key, sum(vals)) for key, vals in g.items())
+			# sorted_g1 = sorted(g1.items(), key=operator.itemgetter(1))
+		if party == 'dem':
+			if raw==True:
+				if sort_ascending ==True:
+					#dem1 = sorted_d1.items()
+					sorted_d1 = sorted(d1.items(), key=operator.itemgetter(1))
+					return sorted_d1
+		if party == 'gop':
+			if raw==False:
+				if sort_ascending ==True:
+					#g1.update((x, ((y/129911581)*100) for (x, y) in g1.items())
+					return g
+		if party == 'dem':
+			if raw==True:
+				if sort_ascending ==False:
+					sorted_d2 = sorted(d1.items(), key=operator.itemgetter(1,0), reverse=True)
+					return sorted_d2
+
+
+
+
 
 	# build the appropriate list of tuples to return
 
-	return [('A', 1), ('B', 2)] 
+	return [('A', 1), ('B', 2)]
+
+#print(get_data())
 
 if __name__ == "__main__":
 
@@ -27,10 +76,10 @@ if __name__ == "__main__":
 	if data[0] == ('WY', 55949.0) and data[-1] == ('CA', 7362490.0):
 		points += 3.33
 
-	data = get_data(party='gop', raw=False)
-	if data [0][0] == 'DC' and int(data[0][1] * 100) == 4 and \
-		data[-1][0] == 'WY' and int(data[-1][1] * 100) == 70:
-		points += 3.33
+	# data = get_data(party='gop', raw=False)
+	# if data [0][0] == 'DC' and int(data[0][1] * 100) == 4 and \
+	# 	data[-1][0] == 'WY' and int(data[-1][1] * 100) == 70:
+	# 	points += 3.33
 
 	data = get_data(party='dem', raw=True, sort_ascending=False)
 	if data[0] == ('CA', 7362490.0) and data[-1] == ('WY', 55949.0):
